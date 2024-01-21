b917086b1b7b700a396fc54adbf7de46b15629fa
SuDenDimon <94171603+SuDenDimon@users.noreply.github.com>
2024-01-21 17:34:06 +0200

    task-3+

---
+++ js/task-3.js
@@ -0,0 +1,24 @@
+function filterArray(numbers, value) {
+    const newMass = [];
+    for (let i = 0; i < numbers.length; i += 1) {
+        const number = numbers[i];
+        if (number > value) {
+            newMass.push(number);
+        } 
+    }
+    return newMass;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     if (number > value) {
+//       newMass.push(index);
+//     }
+//   }
+//   return newMass;
+// }
